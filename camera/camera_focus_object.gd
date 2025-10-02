a86e44fe380dd1c2605c362567e44cfed1ed7390
Lestavol <diepthethinh@gmail.com>
2025-10-03 04:17:54 +0700

    grapple only interact with hookable objects. E key to hook. Still needs visual inidicators

--- camera/camera_focus_object.gd
+++ camera/camera_focus_object.gd
@@ -1,9 +1,12 @@
+class_name CameraFocusObject
 extends Node2D
-## Script that defines the behavior of an invisible Node that follows the player, but have slightly
-## different behaviors so camera can follow this object instead of the player
+## Script that defines the behavior of an invisible Node that generallu follows the player, but have
+## different focuses depneding on context so camera can follow this object instead of the player
 
 @export var lookahead_x_distance: float = 50.0
 @export var lookahead_y_distance: float = 30.0
+
+var grapple_controller: GrappleController = null
 
 var _player: Player
 var _in_air: bool = false
@@ -21,13 +24,22 @@ func _process(_delta: float) -> void:
 	else:
 		_in_air = true
 	
+	apply_focus()
+	global_position = _current_position                 
+
+
+## Defines the focus of the camera
+func apply_focus() -> void:
+	# Horizontal movement
 	if not is_equal_approx(_player.velocity.x, 0.0):
 		_current_position.x = _player.global_position.x + sign(_player.velocity.x) * lookahead_x_distance
 	else:
 		_current_position.x = lerpf(_current_position.x, _player.global_position.x, 0.1)
 	
+	# Vertical movement
 	if not _in_air:
 		_current_position.y = _player.global_position.y
 	
-	
-	global_position = _current_position                 
+	# Grappling action:
+	if grapple_controller and grapple_controller.launched:
+		_current_position = grapple_controller.target
