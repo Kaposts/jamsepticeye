3325a40ff1db125ffaa420fe6b84a095e4229352
Lestavol <diepthethinh@gmail.com>
2025-10-05 03:04:17 +0700

    changed character center camera state to have lookahead

--- camera/states/player_centered_state.gd
+++ camera/states/player_centered_state.gd
@@ -4,4 +4,9 @@ extends CameraState
 
 
 func apply_focus() -> void:
-	_current_position = _player.global_position
+	_current_position.y = _player.global_position.y
+	
+	if is_equal_approx(_player.velocity.x, 0.0):
+		_current_position.x = _player.global_position.x
+	else:
+		_current_position.x = _player.global_position.x + sign(_player.velocity.x) * _camera.lookahead_x_distance
