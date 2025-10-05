dc9831dcd0989615d03ee37a0ca870013ceddc00
Cabage <kaposts.artis1@gmail.com>
2025-10-06 00:07:55 +0300

    pause functionality

--- core/signal_bus.gd
+++ core/signal_bus.gd
@@ -6,3 +6,5 @@ signal player_spawned
 
 signal sig_level_finished
 signal sig_score_submitted
+signal sig_game_paused
+signal sig_game_unpaused
--- menus/pause_menu.gd
+++ menus/pause_menu.gd
@@ -18,22 +18,39 @@ func _ready() -> void:
 	quit_button.pressed.connect(_on_quit_button_pressed)
 
 
+
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		if visible:
+			SignalBus.sig_game_unpaused.emit()
+			hide()
+		else:
+			SignalBus.sig_game_paused.emit()
+			show()
+	
+
 func _on_close_button_pressed() -> void:
 	scroll_anim.play("Scroll_Close")
 	menu_anim.play("Menu_Close")
 	await get_tree().create_timer(1.0).timeout
 	hide()
+	SignalBus.sig_game_unpaused.emit()
 
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_packed(main_menu_scene)
+	SignalBus.sig_game_unpaused.emit()
 
 
 func _on_quit_button_pressed() -> void:
-	var platform_name: StringName = OS.get_name()
-	match platform_name:
-		"Windows":
-			get_tree().quit()
-		"Web":
-			var quit_screen_instance: CanvasLayer = web_quit_screen.instantiate()
-			add_child(quit_screen_instance)
+	get_tree().reload_current_scene()
+	SignalBus.sig_game_unpaused.emit()
+
+	# var platform_name: StringName = OS.get_name()
+	# match platform_name:
+	# 	"Windows":
+	# 		get_tree().quit()
+	# 	"Web":
+	# 		var quit_screen_instance: CanvasLayer = web_quit_screen.instantiate()
+	# 		add_child(quit_screen_instance)
