17bd5f28dfe83896f42f79bbae8957fd15f93af0
Cabage <kaposts.artis1@gmail.com>
2025-10-06 00:56:18 +0300

    merge conflicts fixed

--- menus/pause_menu.gd
+++ menus/pause_menu.gd
@@ -16,6 +16,7 @@ func _ready() -> void:
 	close_button.pressed.connect(_on_close_button_pressed)
 	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
 	quit_button.pressed.connect(_on_quit_button_pressed)
+	scroll_anim.animation_finished.connect(close_finished)
 
 
 
@@ -23,16 +24,13 @@ func _ready() -> void:
 func _input(event):
 	if event.is_action_pressed("pause"):
 		if visible:
-			SignalBus.sig_game_unpaused.emit()
-			hide()
+			close()
 		else:
-			SignalBus.sig_game_paused.emit()
-			show()
+			open()
 	
 
 func _on_close_button_pressed() -> void:
-	scroll_anim.play("Scroll_Close")
-	menu_anim.play("Menu_Close")
+	close()
 	await get_tree().create_timer(1.0).timeout
 	hide()
 	SignalBus.sig_game_unpaused.emit()
@@ -47,6 +45,21 @@ func _on_quit_button_pressed() -> void:
 	get_tree().reload_current_scene()
 	SignalBus.sig_game_unpaused.emit()
 
+
+func close():
+	scroll_anim.play("Scroll_Close")
+	menu_anim.play("Menu_Close")
+
+func close_finished(anim_name):
+	if anim_name == 'Scroll_Close':
+		SignalBus.sig_game_unpaused.emit()
+		hide()
+
+func open():
+	SignalBus.sig_game_paused.emit()
+	show()
+	scroll_anim.play("Scroll_Open")
+	menu_anim.play("Menu_Load")
 	# var platform_name: StringName = OS.get_name()
 	# match platform_name:
 	# 	"Windows":
