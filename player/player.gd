a6b3354fa59dfed7a452cda4e786f13ccfbf4b32
Cabage <kaposts.artis1@gmail.com>
2025-10-05 19:29:10 +0300

    updated level

--- player/player.gd
+++ player/player.gd
@@ -241,7 +241,7 @@ func _sync_animation() -> void:
 		for i in get_slide_collision_count():
 			var c = get_slide_collision(i)
 			if c.get_collider() is RigidBody2D:
-				c.get_collider().apply_central_impulse(-c.get_normal() * 30)
+				c.get_collider().apply_central_impulse(-c.get_normal() * 80)
 
 
 func _set_grounded_state() -> void:
